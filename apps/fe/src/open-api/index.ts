c65910398c84454b40cc8b5905cc0ccb09f856b6
to-long <longth.bka@gmail.com>
2025-12-17 11:23:11 +0700

    adjust api

--- apps/fe/src/open-api/index.ts
+++ apps/fe/src/open-api/index.ts
@@ -2,7 +2,7 @@ import { createPathBasedClient } from 'openapi-fetch';
 import type { paths } from './generated/api';
 
 export const client = createPathBasedClient<paths>({
-  baseUrl: import.meta.env.PUBLIC_API_URL,
+  baseUrl: import.meta.env.PUBLIC_API_URL || 'http://localhost:8000',
 });
 
 // demo call :
